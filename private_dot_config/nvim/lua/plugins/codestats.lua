6cfccd92aaf51137a5d113449be92017e469116c
Vinay Varma <vinay@amo.co>
2025-08-24 10:30:00 +0200

    init chezmoi

---
+++ private_dot_config/nvim/lua/plugins/codestats.lua
@@ -0,0 +1,10 @@
+return {}
+--return {
+--  "vvarma/codestats.nvim",
+--  config = function()
+--    require("codestats-nvim").setup({
+--      token = os.getenv("CODESTATS_API_KEY"),
+--    })
+--  end,
+--  dependencies = { "nvim-lua/plenary.nvim" },
+--}
